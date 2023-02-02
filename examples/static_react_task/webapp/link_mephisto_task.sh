a63710d482cc146b56f3584d7f00b87c82da4489
Jay Huynh <jay.huynh@nib.com.au>
2023-02-03 01:55:04 +1000

    kg-task with newer react version

--- examples/static_react_task/webapp/link_mephisto_task.sh
+++ examples/static_react_task/webapp/link_mephisto_task.sh
@@ -1,2 +1,17 @@
 #!/bin/sh
-npm link mephisto-task
+
+rm -rf node_modules
+rm package-lock.json
+current_dir=$(pwd)
+
+# Link the mephisto-task package
+cd /mephisto/packages/mephisto-task
+npm link
+
+# Link the annotator-tracker package
+cd ../annotated/annotator-tracker
+npm link
+
+cd $current_dir
+npm link mephisto-task @annotated/annotator-tracker
+npm install
